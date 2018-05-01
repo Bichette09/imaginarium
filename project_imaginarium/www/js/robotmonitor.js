195a6ef966c1d4c0b857b8f6ad193a2e32e51edd
Your Name <you@example.com>
2018-05-01 14:01:59 +0000

    .

--- project_imaginarium/www/js/robotmonitor.js
+++ project_imaginarium/www/js/robotmonitor.js
@@ -18,8 +18,8 @@ function onload()
 				"rostraces":[
 					{
 						"name":"angular rot",
-						"topicname":"/imaginarium_core/Measures",
-						"messagetype":"/imaginarium_core/Measures",
+						"topicname":"/imaginarium_core/Gyro",
+						"messagetype":"/imaginarium_core/Gyro",
 						"messagefield":"angular_speed",
 						"fieldindex":2
 					}
@@ -31,52 +31,45 @@ function onload()
 				"rostraces":[
 					{
 						"name":"accel X",
-						"topicname":"/imaginarium_core/Measures",
-						"messagetype":"/imaginarium_core/Measures",
+						"topicname":"/imaginarium_core/Gyro",
+						"messagetype":"/imaginarium_core/Gyro",
 						"messagefield":"acceleration",
 						"fieldindex":1
 					},{
 						"name":"accel Y",
-						"topicname":"/imaginarium_core/Measures",
-						"messagetype":"/imaginarium_core/Measures",
+						"topicname":"/imaginarium_core/Gyro",
+						"messagetype":"/imaginarium_core/Gyro",
 						"messagefield":"acceleration",
 						"fieldindex":2
 					}
 				]
 			}
 		});
-	sGraphs.addGraph(
-		{
-			"title":"mem",
+	lDistanceGraphConfig = {
+			"title":"ultrasound",
 			"y1":{
-				"unit":"%",
-				"range":[-0.1,100.1],
+				"unit":"cm",
+				"range":[-0.1,400.1],
 				"rostraces":[
-					{
-						"name":"mem",
-						"topicname":"/hardware_monitor",
-						"messagetype":"/hardware_monitor/msg",
-						"messagefield":"memload"
-					}
+					
 				]
 			}
-		});
+		}
+	for(var i = 0 ; i < 9 ; ++i)
+	{
+		lDistanceGraphConfig["y1"]["rostraces"].push(
+				{
+						"name":i.toString(),
+						"topicname":"/imaginarium_core/Ultrasound",
+						"messagetype":"/imaginarium_core/Ultrasound",
+						"messagefield":"distance",
+						"fieldindex":i
+					});
+	}
+	
 	sGraphs.addGraph(
-		{
-			"title":"temp",
-			"y1":{
-				"unit":"°C",
-				"range":[-0.1,100.1],
-				"rostraces":[
-					{
-						"name":"temp",
-						"topicname":"/hardware_monitor",
-						"messagetype":"/hardware_monitor/msg",
-						"messagefield":"cputemp"
-					}
-				]
-			}
-		});
+		lDistanceGraphConfig);
+	
 	
 	sGraphs.updateSizes();
 }
