161f9f45a2fa11d552889ebd4e7df2c0814831c2
CaroIncha <35765628+CaroIncha@users.noreply.github.com>
2018-05-01 17:39:06 +0200

    Update antenna.py

--- src/imaginarium/imaginarium_core/nodes/antenna.py
+++ src/imaginarium/imaginarium_core/nodes/antenna.py
@@ -15,7 +15,7 @@ class Antenna:
 	
 		self.gpioB.on()
 
-		self.seen_impulse = 0
+		self.seen_impulse = false
 
 		self.gpioA.when_pressed = lambda *args : self.pulse(self.gpioA, 1)
 
@@ -23,7 +23,7 @@ class Antenna:
 
 	def pulse(self, gpio, level):
 		self.gpioB.off()
-		self.seen_impulse = 1
+		self.seen_impulse = true
 		self.currentTime = time.time()
 		print('pulse')
 
@@ -32,21 +32,26 @@ class Antenna:
 			self.gpioB.on()
 			self.currentTime = None
 		seen_impulse = self.seen_impulse
-		self.seen_impulse = 0
-  return seen_impulse 
+		self.seen_impulse = false
+  		return seen_impulse 
 
 if __name__ == "__main__":
 	
 	os.getcwd()
 	rospy.init_node('antenna')
-
+	
+	LPreviousStatus = false;
 	
 	sRosPublisher = rospy.Publisher('imaginarium_core/Antenna', imaginarium_core.msg.Antenna, queue_size=5)
         lAntennaInterruption = Antenna(rospy.get_param('/antenna/pinA'),rospy.get_param('/antenna/pinB'))
 
 	while not rospy.core.is_shutdown():
 	
-
-	
-		# Message publication
-  sRosPublisher.publish(imaginarium_core.msg.Antenna())
+		if lAntennaInterruption.getAndReset() is not LPreviousStatus:
+		#Alors on vient de voir passer un front montant ou descendant
+			# Message publication
+			LPreviousStatus = not LPreviousStatus
+			sRosPublisher.publish(imaginarium_core.msg.Antenna(LPreviousStatus))
+		else:
+			sleep(0.1)
+		
