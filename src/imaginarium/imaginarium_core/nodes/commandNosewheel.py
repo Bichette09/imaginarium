9824e68e113e40c384d989928c2f66183ee61584
CaroIncha <35765628+CaroIncha@users.noreply.github.com>
2018-05-01 18:29:49 +0200

    Update commandNosewheel.py

--- src/imaginarium/imaginarium_core/nodes/commandNosewheel.py
+++ src/imaginarium/imaginarium_core/nodes/commandNosewheel.py
@@ -7,8 +7,17 @@ import os
 import serial
 
 class CommandNosewheel(object):
-
- 
+	
+	def __init__(self):
+		self.speed = 0
+		self.antenna = false
+		self.ultrason= [0]*10
+	
+ 	def updateSpeed(self,param):
+		self.speed = param.speed
+	
+	def updateUltrason(self,param):
+		self.ultrason = param.
 
 if __name__ == "__main__":
 	
@@ -16,12 +25,15 @@ if __name__ == "__main__":
 	rospy.init_node('commandNosewheel')
 
 	
-	sRosPublisher = rospy.Publisher('imaginarium_core/CommandNosewheel', imaginarium_core.msg.CommandNosewheel, queue_size=5)
-        lUltrasoundReader = Ultrasound(rospy.get_param('/commandNosewheel/serialPort'))
-
+	sRosPublisher = rospy.Publisher('imaginarium_core/CommandNosewheel', imaginarium_core.msg.CommandNosewheel, )
+	lCommandNosewheel = CommandNosewheel(rospy.get_param('/commandNosewheel/serialPort'))
+	sRosSuscriberUltra = rospy.Subscriber('imaginarium_core/Ultrasound', imaginarium_core.msg.Ultrasound,)
+	sRosSuscriberSpeed = rospy.Subscriber('imaginarium_core/Speed', imaginarium_core.msg.Speed,lCommandNosewheel.updateSpeed)
+	sRosSuscriberAntenna = rospy.Subscriber('imaginarium_core/Antenna', imaginarium_core.msg.Antenna, )
+	
 	while not rospy.core.is_shutdown():
 	
 
 	
 		# Message publication
-sRosPublisher.publish(imaginarium_core.msg.CommandNosewheel(lNoseWheelAngle))
+	sRosPublisher.publish(imaginarium_core.msg.CommandNosewheel(lNoseWheelAngle))
