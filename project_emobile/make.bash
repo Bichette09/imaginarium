89900a09044dff1498e5ef48eaa9a4b4e2266015
Bichette09 <lead_overdrive@hotmail.fr>
2018-06-02 12:40:51 +0200

    --

--- project_emobile/make.bash
+++ project_emobile/make.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ..
-catkin_make --pkg settings_store hardware_monitor imaginarium_core
+EXPORT LEDDAR_LIB_DIR=~/leddar_sdk
+catkin_make --pkg settings_store hardware_monitor imaginarium_core leddar
