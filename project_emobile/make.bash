a00dd0ce76c9e152988d7474da5627be0ec141d5
eMobile <you@example.com>
2018-06-30 19:21:04 +0000

    vu8 integration

--- project_emobile/make.bash
+++ project_emobile/make.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd ..
-EXPORT LEDDAR_LIB_DIR=~/leddar_sdk
-catkin_make --pkg settings_store hardware_monitor imaginarium_core leddar
+catkin_make --pkg settings_store hardware_monitor emaginarium_common emobile
