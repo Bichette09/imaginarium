d22f7e4b26edd1b9859a83f6c8812a225b4574b3
eMobile <you@example.com>
2018-07-29 16:03:33 +0000

    fix point clound node

--- src/emaginarium/emobile/nodes/pointCloud.py
+++ src/emaginarium/emobile/nodes/pointCloud.py
@@ -5,47 +5,71 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from emobile.msg import PointCloud
 from numpy import pi
+import math
 
-xVu8 = [0]*8
-yVu8 = [0]*8
-xM16 = [0]*16
-yM16 = [0]*16
 
-def rot(x,y,angle):
-    for i in range(len(x)):
-        x[i] = x[i]*cos(angle)-y[i]*sin(angle)
-        y[i] = x[i]*sin(angle)+y[i]*cos(angle)
-        
-def translate(x,y,xoffset,yoffset):
-    for i in range(len(x)):
-        x[i] = x[i]+xoffset
-        y[i] = y[i]+yoffset
+class PointCloudConverter():
+	
+	def __init__(self,pPublisher):
+		self.xVu8 = [0]*8
+		self.yVu8 = [0]*8
+		self.dVu8 = [0]*8
+		self.xM16 = [0]*16
+		self.yM16 = [0]*16
+		self.dM16 = [0]*16
+		self.mPublisher = pPublisher
+		
+	def sendPointCloud(self):
+		msg = PointCloud()
+		
+		msg.distance = self.dVu8 + self.dM16
+		msg.x1 = self.xVu8 + self.xM16
+		msg.y1 = self.yVu8 + self.yM16
+		
+		self.mPublisher.publish(msg)
 
-def Vu8PointCloud(scan):
-    angle = scan.angle_min
-    for i,dist in enumerate(scan.ranges):
-        angle += i*scan.angle_increment
-        xVu8[i] = dist*cos(angle)
-        yVu8[i] = dist*sin(angle)
-    rot(xVu8,yVu8,-pi/2)
-    translate(xVu8,yVu8)
+	def rot(self,x,y,angle):
+		for i in range(len(x)):
+			x[i] = x[i]*math.cos(angle)-y[i]*math.sin(angle)
+			y[i] = x[i]*math.sin(angle)+y[i]*math.cos(angle)
+			
+	def translate(self,x,y,xoffset,yoffset):
+		for i in range(len(x)):
+			x[i] = x[i]+xoffset
+			y[i] = y[i]+yoffset
 
-def M16PointCloud(scan):
-    angle = scan.angle_min
-    for i,dist in enumerate(scan.ranges):
-        angle += i*scan.angle_increment
-        xM16[i] = dist*cos(angle)
-        yM16[i] = dist*sin(angle)
-        
-        
-    
+	def Vu8PointCloud(self,scan):
+		angle = scan.angle_min
+		for i,dist in enumerate(scan.ranges):
+			angle += i*scan.angle_increment
+			self.xVu8[i] = dist*math.cos(angle)
+			self.yVu8[i] = dist*math.sin(angle)
+			self.dVu8[i] = dist
+		self.rot(self.xVu8,self.yVu8,-pi/2)
+		self.translate(self.xVu8,self.yVu8,0.,0.)
+		self.sendPointCloud()
+		
 
-# init_ros_node
-rospy.init_node('leddar')
-SubFrontLeddar = rospy.Subscriber('/leddarVu8', LaserScan,Vu8PointCloud)
-SubFrontLeddar = rospy.Subscriber('/leddarM16', LaserScan,M16PointCloud)
-pub = rospy.Publisher('/pointcloud', PointCloud,queue_size = 10)
+	def M16PointCloud(self,scan):
+		angle = scan.angle_min
+		for i,dist in enumerate(scan.ranges):
+			angle += i*scan.angle_increment
+			self.xM16[i] = dist*math.cos(angle)
+			self.yM16[i] = dist*math.sin(angle)
+			self.dM16[i] = dist
+		self.sendPointCloud()
 
-while not rospy.is_shutdown():
-    
-	pub.publish(msg)
+	
+if __name__ == "__main__":
+	# init_ros_node
+	rospy.init_node('leddar')
+	
+	pub = rospy.Publisher('/pointcloud', PointCloud,queue_size = 10)
+	lPointCloudConverter = PointCloudConverter(pub)
+
+	
+	SubFrontLeddar = rospy.Subscriber('/leddarVu8', LaserScan,lPointCloudConverter.Vu8PointCloud)
+	SubFrontLeddar = rospy.Subscriber('/leddarM16', LaserScan,lPointCloudConverter.M16PointCloud)
+	
+	rospy.spin() 
+	
