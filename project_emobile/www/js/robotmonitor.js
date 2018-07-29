9c6a542538b91d30534515cd6eb7bb790ce0df86
eMobile <you@example.com>
2018-07-29 16:36:39 +0000

    add some monitoring

--- project_emobile/www/js/robotmonitor.js
+++ project_emobile/www/js/robotmonitor.js
@@ -9,59 +9,24 @@ function onload()
 	
 	// create graphs
 	sGraphs.init('graphs');
-	sGraphs.addGraph(
-		{
-			"title":"gyro",
-			"y1":{
-				"unit":"°/sec",
-				"range":[-10,10],
-				"rostraces":[
-					{
-						"name":"angular rot",
-						"topicname":"/imaginarium_core/Gyro",
-						"messagetype":"/imaginarium_core/Gyro",
-						"messagefield":"angular_speed",
-						"fieldindex":2
-					}
-				]
-			},
-			"y2":{
-				"unit":"m/s-2",
-				"range":[-5,5],
-				"rostraces":[
-					{
-						"name":"accel X",
-						"topicname":"/imaginarium_core/Gyro",
-						"messagetype":"/imaginarium_core/Gyro",
-						"messagefield":"acceleration",
-						"fieldindex":1
-					},{
-						"name":"accel Y",
-						"topicname":"/imaginarium_core/Gyro",
-						"messagetype":"/imaginarium_core/Gyro",
-						"messagefield":"acceleration",
-						"fieldindex":2
-					}
-				]
-			}
-		});
+	
 	lDistanceGraphConfig = {
-			"title":"ultrasound",
+			"title":"vu8",
 			"y1":{
 				"unit":"cm",
-				"range":[-0.1,400.1],
+				"range":[-0.1,4.1],
 				"rostraces":[
 					
 				]
 			}
 		}
-	for(var i = 0 ; i < 10 ; ++i)
+	for(var i = 0 ; i < 8 ; ++i)
 	{
 		lDistanceGraphConfig["y1"]["rostraces"].push(
 				{
 						"name":i.toString(),
-						"topicname":"/imaginarium_core/Ultrasound",
-						"messagetype":"/imaginarium_core/Ultrasound",
+						"topicname":"/pointcloud",
+						"messagetype":"/emobile/PointCloud",
 						"messagefield":"distance",
 						"fieldindex":i
 					});
@@ -79,70 +44,33 @@ function onload()
 					*/
 	sGraphs.addGraph(
 		{
-			"title":"speed/dir",
+			"title":"actuator",
 			"y1":{
-				"unit":"m/sec",
-				"range":[-4,4],
+				"unit":"power%",
+				"range":[-1.01,1.01],
 				"rostraces":[
 					{
-						"name":"speed",
-						"topicname":"/imaginarium_core/Speed",
-						"messagetype":"/imaginarium_core/Speed",
-						"messagefield":"speed"
+						"name":"power",
+						"topicname":"/emobile/CommandThrottle",
+						"messagetype":"/emobile/CommandThrottle",
+						"messagefield":"throttle"
 					}
 				]
 			},
 			"y2":{
 				"unit":"°",
-				"range":[-45,45],
+				"range":[-1.01,1.01],
 				"rostraces":[
 					{
 						"name":"wheel angle",
-						"topicname":"/imaginarium_core/CommandNosewheel",
-						"messagetype":"/imaginarium_core/CommandNosewheel",
-						"messagefield":"nosewheelAngle"
+						"topicname":"/emobile/CommandSteering",
+						"messagetype":"/emobile/CommandSteering",
+						"messagefield":"steering"
 					}
 				]
 			}
 		});
 		
-	sGraphs.addGraph(
-		{
-			"title":"wheel_cmd",
-			/*"range":[-4,4],*/
-			"y1":{
-				"unit":"?",
-				"rostraces":[
-					{
-						"name":"propcommand",
-						"topicname":"/imaginarium_core/CommandNosewheel",
-						"messagetype":"/imaginarium_core/CommandNosewheel",
-						"messagefield":"propCommand"
-					},{
-						"name":"preccommand",
-						"topicname":"/imaginarium_core/CommandNosewheel",
-						"messagetype":"/imaginarium_core/CommandNosewheel",
-						"messagefield":"precCommand"
-					}
-				]
-			},
-			"y2":{
-				"unit":"°",
-				"rostraces":[
-					{
-						"name":"left_angle",
-						"topicname":"/imaginarium_core/CommandNosewheel",
-						"messagetype":"/imaginarium_core/CommandNosewheel",
-						"messagefield":"leftAngle"
-					},{
-						"name":"right_angle",
-						"topicname":"/imaginarium_core/CommandNosewheel",
-						"messagetype":"/imaginarium_core/CommandNosewheel",
-						"messagefield":"rightAngle"
-					}
-				]
-			}
-		});
 	
 	sGraphs.updateSizes();
 }
