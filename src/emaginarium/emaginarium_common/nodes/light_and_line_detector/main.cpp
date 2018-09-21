960bf21eefe27aa6062bbb867fd171f27a78fae5
eMobile <you@example.com>
2018-09-21 09:59:53 +0000

    -

--- src/emaginarium/emaginarium_common/nodes/light_and_line_detector/main.cpp
+++ src/emaginarium/emaginarium_common/nodes/light_and_line_detector/main.cpp
@@ -127,7 +127,10 @@ int main(int argc, char ** argv)
 		settings_store::StateDeclarator lStateDeclarator(n);
 		
 #if 1
-		CameraFrameProvider lFrameProvider(CameraFrameProvider::Parameters(320*2,240*2,10));
+		CameraFrameProvider lFrameProvider(
+				CameraFrameProvider::Parameters(320*2,240*2,10)
+				//CameraFrameProvider::Parameters(1280/2,720/2,10)
+			);
 #else
 		VideoFrameProvider lFrameProviderA(VideoFrameProvider::Parameters(640,480,24,"/home/pi/Untitled Project.avi"));
 		PauseProxyFrameProvider lFrameProvider(lFrameProviderA,n);
