1a5ef11145e18bfe802eb705a9fcfcdea28a492a
eMobile <you@example.com>
2019-07-28 14:13:32 +0000

    add gyto

--- src/emaginarium/emaginarium_common/nodes/gyro.py
+++ src/emaginarium/emaginarium_common/nodes/gyro.py
@@ -8,35 +8,34 @@ import smbus
 import time
 
 # define MPU address
-sMpuAddress = 0x69
+
 class Gyro(object):
-	def __init__(self, pIcPort):
+	def __init__(self, pIcPort, pMpuAddress):
+		self.__mMpuAddress = pMpuAddress
 		try:
 			self.__mI2cPort = smbus.SMBus(pIcPort)
 			# wake up device and init power management
-			self.__mI2cPort.write_byte_data(sMpuAddress,0x6B,0x00)# PWR_MGMT_1
-			
+			self.__mI2cPort.write_byte_data(self.__mMpuAddress,0x6B,0x00)# PWR_MGMT_1
 			# who am i
-			lWhoAmI = self.__mI2cPort.read_byte_data(sMpuAddress,0x75)
+			lWhoAmI = self.__mI2cPort.read_byte_data(self.__mMpuAddress,0x75)
 			if lWhoAmI != 0x68:
 				rospy.logerr('Fail to init gyro ' + str(lWhoAmI))
 				raise Exception()
-
 			# configure gyro
-			self.__mI2cPort.write_byte_data(sMpuAddress,0x1B,0x00) # range @ +/- 250°/s
+			self.__mI2cPort.write_byte_data(self.__mMpuAddress,0x1B,0x00) # range @ +/- 250°/s
 			self.__mAngularSpeedFactor = 250. * 1. / 32768.
 			
 			# configure accelero
-			self.__mI2cPort.write_byte_data(sMpuAddress,0x1C,0x00) # range +/- 2g (we are not in a jet fighter)
+			self.__mI2cPort.write_byte_data(self.__mMpuAddress,0x1C,0x00) # range +/- 2g (we are not in a jet fighter)
 			self.__mAccelFactor = 2. * 1. / 32768.
 		except:
 			self.__mI2cPort = None
-			rospy.logerr('Fail to initialize gyro')
+			rospy.logerr('Fail to initialize gyro on port %d @%x' % (pIcPort,self.__mMpuAddress))
 	
 	def readAccel(self):
 		if self.__mI2cPort is None:
 			return [0,0,0]
-		lAccelData = self.__mI2cPort.read_i2c_block_data(sMpuAddress,0x3B,6)
+		lAccelData = self.__mI2cPort.read_i2c_block_data(self.__mMpuAddress,0x3B,6)
 		lXAccel = lAccelData[0] * 256 + lAccelData[1]
 		if lXAccel > 32767:
 			lXAccel -= 65536
@@ -54,7 +53,7 @@ class Gyro(object):
 	def readAngularSpeed(self):
 		if self.__mI2cPort is None:
 			return [0,0,0]
-		lGyroData = self.__mI2cPort.read_i2c_block_data(sMpuAddress,0x43,6)
+		lGyroData = self.__mI2cPort.read_i2c_block_data(self.__mMpuAddress,0x43,6)
 		lXGyro = lGyroData[0] * 256 + lGyroData[1]
 		if lXGyro > 32767:
 			lXGyro -= 65536
@@ -77,7 +76,7 @@ if __name__ == "__main__":
 
 	
 	sRosPublisher = rospy.Publisher('emaginarium_common/Gyro', emaginarium_common.msg.Gyro, queue_size=5)
-	lGyroReader = Gyro(rospy.get_param('/gyro/i2cPort'))
+	lGyroReader = Gyro(rospy.get_param('/gyro/i2cPort'),rospy.get_param('/gyro/sMpuAddress'))
 
 	while not rospy.core.is_shutdown():
 	
